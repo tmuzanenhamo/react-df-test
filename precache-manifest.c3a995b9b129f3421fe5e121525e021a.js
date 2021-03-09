7f9a0139f9e0e3b8fa89d5a3f8c3dc70a36ab00d
tmuzanenhamo <tawandamuzanenhamo14@gmail.com>
2021-03-09 20:51:34 +0200

    Updates

---
+++ precache-manifest.c3a995b9b129f3421fe5e121525e021a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f898691cd6cdf8c91ea63778d012bac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4fa5c707d4583159e58",
+    "url": "/static/css/main.52eeca4a.chunk.css"
+  },
+  {
+    "revision": "e9d5e493f93a42254f60",
+    "url": "/static/js/2.a69e25bc.chunk.js"
+  },
+  {
+    "revision": "51325446a6934f12d709f84d380f7fe2",
+    "url": "/static/js/2.a69e25bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4fa5c707d4583159e58",
+    "url": "/static/js/main.1522ade5.chunk.js"
+  },
+  {
+    "revision": "79969ee6fa98bba36af0",
+    "url": "/static/js/runtime-main.c529592b.js"
+  },
+  {
+    "revision": "16bcbe8df3b9d5dbf556397864c80153",
+    "url": "/static/media/logo.16bcbe8d.png"
+  }
+]);
